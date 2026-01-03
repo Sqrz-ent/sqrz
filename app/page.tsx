da2baeb1edec94fdb55f8c1b7ab89b0743096202
patrick wilhelm <patrickwilhelm@SQRZ-Air.local>
2026-01-03 23:48:57 +0100

    pixel

--- app/page.tsx
+++ app/page.tsx
@@ -117,35 +117,36 @@ export default async function HomePage() {
   const spotifyEmbed = profile.spotify_url
     ? getSpotifyEmbedUrl(profile.spotify_url)
     : null;
-
-  return (
-    <main
+return (
+  <main
     style={{
-        minHeight: "100vh",
-        background: "#000",
-        padding: "20px 10px",
-      }}
-          {profile.facebook_pixel_id && (
-  <>
-    {/* Facebook Pixel */}
-    <Script id="facebook-pixel" strategy="afterInteractive">
-      {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '${profile.facebook_pixel_id}');
-        fbq('track', 'PageView');
-      `}
-    </Script>
-  </>
-)}
-      
-    >
+      minHeight: "100vh",
+      background: "#000",
+      padding: "20px 10px",
+    }}
+  >
+    {profile.facebook_pixel_id && (
+      <>
+        {/* Facebook Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${profile.facebook_pixel_id}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </>
+    )}
+
+  
+    
       <div
         style={{
           maxWidth: 520,
