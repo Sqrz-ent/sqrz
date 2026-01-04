1bd65f798f60e6c4220d80e7dbe47b77036c075a
patrick wilhelm <patrickwilhelm@SQRZ-Air.local>
2026-01-04 19:28:25 +0100

    color

--- components/BookMeButton.tsx
+++ components/BookMeButton.tsx
@@ -25,7 +25,7 @@ const buttonStyle = {
   padding: "12px 18px",
   borderRadius: 999,
   border: "none",
-  background: "accent-color",
+  background: "text-accent",
   color: "#000",
   fontWeight: 700,
   cursor: "pointer",
