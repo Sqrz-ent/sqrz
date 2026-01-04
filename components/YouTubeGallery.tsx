88491849647648be1c59bba4cc125b403e3a3121
patrick wilhelm <patrickwilhelm@SQRZ-Air.local>
2026-01-04 18:29:18 +0100

    white

--- components/YouTubeGallery.tsx
+++ components/YouTubeGallery.tsx
@@ -60,7 +60,7 @@ export default function YouTubeGallery({
               border: "none",
               cursor: "pointer",
               background:
-                index === activeIndex ? "#dedede55" : "#e2e2e278",
+                index === activeIndex ? "#dedede1e" : "#e2e2e278",
               color: "#fff",
               textAlign: "left",
               opacity: index === activeIndex ? 1 : 0.7,
