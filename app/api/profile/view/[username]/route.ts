417a39d06a04af3aca219040154519f56d500c8b
patrick wilhelm <patrickwilhelm@SQRZ-Air.local>
2026-01-11 03:35:44 +0100

    new route ts

--- app/api/profile/view/[username]/route.ts
+++ app/api/profile/view/[username]/route.ts
@@ -2,15 +2,18 @@ import { NextResponse } from "next/server";
 
 export async function POST(
   req: Request,
-  { params }: { params: { username: string } }
+  context: { params: Promise<{ username: string }> }
 ) {
+  const { username } = await context.params;
+
   const res = await fetch(
-    `https://xuwq-ib46-ag3b.f2.xano.io/api:ZUfHfBuE/profileCounter/${params.username}`,
+    `https://xuwq-ib46-ag3b.f2.xano.io/api:ZUfHfBuE/profileCounter/${username}`,
     {
       method: "POST",
     }
   );
 
-  const data = await res.text();
-  return NextResponse.json({ ok: true, data });
+  const text = await res.text();
+
+  return NextResponse.json({ ok: true, data: text });
 }
