a8cc06b3f54ba0f1d334c2a6cf4af7d92dcc0b98
patrick wilhelm <patrickwilhelm@SQRZ-Air.local>
2026-01-11 03:06:53 +0100

    syntax

--- components/ViewTracker.tsx
+++ components/ViewTracker.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
-export default function ViewTracker({ slug }: { slug: string }) {
+export default function ViewTracker({ username }: { username: string }) {
   const searchParams = useSearchParams();
   const isPreview = searchParams.get("preview") === "1";
 
@@ -11,11 +11,12 @@ export default function ViewTracker({ slug }: { slug: string }) {
     if (isPreview) return;
 
     fetch(
-        `https://xuwq-ib46-ag3b.f2.xano.io/api:ZUfHfBuE/profileCounter/${username}`
-        {
-      method: "POST",
-    });
-  }, [isPreview, slug]);
+      `https://xuwq-ib46-ag3b.f2.xano.io/api:ZUfHfBuE/profileCounter/${username}`,
+      {
+        method: "POST",
+      }
+    );
+  }, [isPreview, username]);
 
   return null;
 }
