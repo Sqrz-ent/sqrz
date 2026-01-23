15f3a8a6f0031d6d414e6bc52149f6d0aa5bb599
patrick wilhelm <patrickwilhelm@SQRZ-Air.local>
2026-01-23 03:42:40 +0100

    limit shows

--- components/BandsintownWidget.tsx
+++ components/BandsintownWidget.tsx
@@ -32,6 +32,8 @@ export default function BandsintownWidget({ bandsintownUrl }: BandsintownWidgetP
     a.setAttribute("data-background-color", "transparent");
     a.setAttribute("data-separator-color", "rgba(255, 255, 255, 0.73)");
     a.setAttribute("data-auto-style", "false");
+    a.setAttribute("data-text-alignment", "left");
+    a.setAttribute("data-display-limit", "4");
 
 
 
