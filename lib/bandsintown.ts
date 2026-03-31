9215bdd3d9ded3563d07f58547c183654a6b9662
Will Villa <will@sqrz.com>
2026-04-01 01:03:53 +0200

    fix: bandsintown accepts URL or plain name

--- lib/bandsintown.ts
+++ lib/bandsintown.ts
@@ -1,4 +1,7 @@
 export function getBandsintownArtistFromUrl(url?: string | null) {
   if (!url) return null;
+  if (url.includes("bandsintown.com/a/")) {
+    return url.split("/a/")[1].split("?")[0].split("/")[0];
+  }
   return url.trim();
 }
